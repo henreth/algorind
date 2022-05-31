eeed337163b989a2c67e402d0f839ff5221a89c1
Henry Miller <mail.henrym+git@gmail.com>
2022-05-30 23:00:52 -0400

    226

--- week1/0530/121-besttimetobuyandsellstock/121.ts
+++ week1/0530/121-besttimetobuyandsellstock/121.ts
@@ -15,3 +15,20 @@ function maxProfit(prices: number[]): number {
 };
 
 
+// var maxProfit = function(prices) {
+//     let profit = 0;
+//     let min = prices[0];
+    
+//     for(let i = 1; i < prices.length; ++i) { 
+//         if(min > prices[i]) {
+//             min = prices[i];
+//         } else if(prices[i] - min > profit) {
+//             profit = prices[i] - min;
+//         }
+//     }
+    
+//     return profit;
+// };
+
+
+
